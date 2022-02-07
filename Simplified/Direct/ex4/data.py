7d2aed9c4e11f32cfbfacabc12de1f6bb40e47cb
tranquockinh <tqkinh12@gmail.com>
2022-02-07 23:55:25 +0800

    supperposition with two layer models

---
+++ Simplified/Direct/ex4/data.py
@@ -0,0 +1,12 @@
+
+import numpy as np
+import sympy as sp
+import matplotlib.pyplot as plt
+
+shear_wave = [150, 400]
+depth = [4, 10]
+list_wavelength = np.arange(2, 30, 0.5)
+alpha = 0.5
+at_iter = 4
+this_function = 2
+case = 'two_layer'
